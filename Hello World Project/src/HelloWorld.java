586835cf7431212ee2002e59c94bcca7a11a1f4d
APCS3rd <APCS3rd@MHS-LT9-09>
2019-08-20 10:12:16 -0700

    Complete Hello World Project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Kardon Lam 8/16/19
+//3
+//First class (iconic) aka program written in Java for APCS A
+//Prints message to the screen
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
