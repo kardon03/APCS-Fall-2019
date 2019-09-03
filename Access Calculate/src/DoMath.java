2098f5cc666fb060aa4f5b82a484d339891a2022
APCS3rd <APCS3rd@MHS-LT9-09>
2019-09-03 10:29:58 -0700

    80 percent of part 1

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,7 +7,10 @@ public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(2));
 		System.out.println(Calculate.cube(2));
-		System.out.println(Calculate.average(3));
+		System.out.println(Calculate.average(2, 3));
+		System.out.println(Calculate.average(2, 3, 4));
+		System.out.println(Calculate.toDegrees(2));
+		System.out.println(Calculate.toRadians(2));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -5,14 +5,37 @@
 public class Calculate {
 	//A call to square returns the square of the value passed. The method accepts an integer and returns and integer. 
 	public static int square(int number) {
-		return number*number; 	
+		int answer = number * number;
+		return answer;
 	}
+	//A call to cube returns the cube of the value passed. The method accepts an integer and returns and integer.
 	public static int cube(int number) {
-		return number*number*number;
+		int answer = number * number * number;
+		return answer;
 	}
-	public static int average(double number) {
-		return (number+number)/2;
+	//A call to average returns the average of the values passed to it. This method accepts two doubles and returns a double
+	public static double average(double number1, double number2) {
+		double answer = (number1 + number2)/2;
+		return answer;
+	}
+	//Another average method. This one accepts three doubles and returns a double. The name is the same, the difference is in the header and the calculation. (The ability to define two or more different methods with the same name but different method signatures is called overloading. This average method is an example of an "overload method".)
+	public static double average(double number1, double number2, double number3) {
+		double answer = (number1 + number2 + number3)/3;
+		return answer;
+	}
+	//A call to toDegrees converts an angle measure given in radians into degrees. The method accepts a double and returns a double. The method should use 3.14159 as an approximation for pi.
+	public static double toDegrees(double rad) {
+		double answer = (180/3.14159) * rad;
+		return answer;
+	}
+	//A call to toRadians converts an angle measure given in degrees into radians. The method accepts a double and returns a double. The method should use 3.14159 as an approximation for pi.
+	public static double toRadians(double degree) {
+		double answer = degree * (3.14159/180);
+		return answer;
+	//A call to discriminant provides	
 	}
 }
+
+
 
 
