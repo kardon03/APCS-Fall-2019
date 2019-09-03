bbada7756855c4eaa4e0cdb12a969dc5240ce837
APCS3rd <APCS3rd@MHS-LT9-09>
2019-09-03 09:54:08 -0700

    Part 0 and Parts of Part 1

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+//Kardon Lam
+//APCS Period 3
+//August 29, 2019
+//Call the code from the Calculate class (aka client or runner code)
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average(3));
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,18 @@
+//Kardon Lam
+//APCS Period 3
+//August 29, 2019
+//Library of math functions
+public class Calculate {
+	//A call to square returns the square of the value passed. The method accepts an integer and returns and integer. 
+	public static int square(int number) {
+		return number*number; 	
+	}
+	public static int cube(int number) {
+		return number*number*number;
+	}
+	public static int average(double number) {
+		return (number+number)/2;
+	}
+}
+
+
